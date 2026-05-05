845f51d61d3fabb33fed7296e21ff84607c59997
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-05-05 23:28:47 +0000

    Signal threads to stop listening for jobs.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -3,7 +3,7 @@ use std::sync::{mpsc, Arc, Mutex};
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -30,18 +30,19 @@ impl ThreadPool {
 
         ThreadPool {
             workers,
-            sender,
+            sender: Some(sender),
         }
     }
 
     pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static, {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
             if let Some(thread) = worker.thread.take() {
@@ -59,15 +60,18 @@ struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver
-                .lock()
-                .expect("unpoisoned mutex")
-                .recv() // https://doc.rust-lang.org/std/sync/mpsc/struct.Receiver.html#method.recv
-                .unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job, executing.");
-
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job, executing.");
+                    job();
+                },
+                Err(_) => {
+                    println!("Worker {id} shutting down.");
+                    break;
+                },
+            }
         });
         Worker { id, thread: Some(thread) }
     }
