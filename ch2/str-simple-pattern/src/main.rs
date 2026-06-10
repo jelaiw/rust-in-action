d05575c77e75bcebb6b843952914f6ea52e9135f
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-06-09 21:25:43 -0500

    Comment on where the quote is from.

--- ch2/str-simple-pattern/src/main.rs
+++ ch2/str-simple-pattern/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let search_term = "picture";
     // Multilined strings do not require special syntax. The \ character on line 4 escapes the new line.
-    let quote = "\
+        let quote = "\
 Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
-What do we seek through millions of pages?";
+What do we seek through millions of pages?"; // From Virginia Woolf 1922 novel Jacob's Room.
 
     // lines() returns an iterator over quote where each iteration is a line of text. Rust uses each
     // operating system’s conventions on what constitutes a new line.
