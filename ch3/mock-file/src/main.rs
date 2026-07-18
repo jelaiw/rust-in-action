a63c076e05f5adfe0c42487a5a3ec673e6ba9c81
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-07-18 18:54:36 +0000

    Code comments from listing 3.2.
    Comment on vec! macro is probably from an earlier version of this code example (which uses Vec::new now) but leaving as is.

--- ch3/mock-file/src/main.rs
+++ ch3/mock-file/src/main.rs
@@ -1,15 +1,20 @@
+// Allows println! to print File.
+// The std::fmt::Debug trait works in conjunction with {:?} within
+// the macro to enable File as a printable string.
 #[derive(Debug)]
 struct File {
     name: String,
-    data: Vec<u8>,
+    data: Vec<u8>, // Using Vec<u8>, provides access to some useful conveniences
+    // like dynamic sizing, which makes it possible to simulate writing to a file.
 }
 
 fn main() {
     let f1 = File {
-        name: String::from("f1.txt"),
-        data: Vec::new(),
+        name: String::from("f1.txt"), // String::from generates owned strings from string literals, which are slices.
+        data: Vec::new(), // Here the vec! macro simulates an empty file.
     };
 
+    // Accessing fields uses the . operator. Accessing fields by reference prevents their use after move issues.
     let f1_name = &f1.name;
     let f1_length = &f1.data.len();
 
